f73a39f233dfa79aa19d81b0bda739c6a86a774a
Zabbbbbbbb <204318@stmichaelcollege.nl>
2025-12-01 23:15:35 +0100

    Add files via upload

--- updater.php
+++ updater.php
@@ -1,5 +1,5 @@
 <?php
-    // Read JSON data
+    //Lezen van de ontvangen JSON data
     $data = json_decode(file_get_contents("php://input"), true);
 
     $pfp = $data["pfp"];
@@ -7,14 +7,14 @@
     $private = $data["private"];
     $moderator_application = $data["moderator_application"];
 
-    // Convert JS boolean to integer for MySQL
+    //Bool naar 1 of 0 omzetten, want dat wil SQL om de een of andere reden
     $private = $private ? 1 : 0;
     $moderator_application = $moderator_application ? 1 : 0;
 
-    // Connect to database
+    //Verbinding maken met de database
     $pdo = new PDO("mysql:host=localhost;dbname=po_webapp", "root", "");
 
-    // Update both fields
+    //De dingen updaten
     $stmt = $pdo->prepare("UPDATE account SET pfp = ?, private = ?, moderator_application = ? WHERE naam = ?");
     $stmt->execute([$pfp, $private, $moderator_application, $username]);
 ?>
